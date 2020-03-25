c08420195da6accda6d381ccee7e4ce7ff66cf0c
BilyiVoron <aborsen1072@gmail.com>
2020-03-25 21:24:37 +0200

    Remove some mistakes & add testing turns

--- lantern/tdd/lonely_robot.py
+++ lantern/tdd/lonely_robot.py
@@ -9,6 +9,8 @@ class Obstacle:
         self.x = x_obstacle
         self.y = y_obstacle
         self.asteroid = asteroid
+        if any([self.asteroid.a < self.x, self.x < 0, self.asteroid.b < self.y, self.y < 0]):
+            raise ObstaclePositionError
 
 
 class Robot:
@@ -19,18 +21,22 @@ class Robot:
         self.direction = direction
         self.obstacle = obstacle
         self.position = x, y
-        if not (self.asteroid.a >= self.x >= 0 and self.asteroid.b >= self.y >= 0):
-            raise MissAsteroidError
-        elif self.x == self.obstacle.x or self.y == self.obstacle.y:
+        if any([self.asteroid.a < self.x, self.x < 0, self.asteroid.b < self.y, self.y < 0]):
+            raise OutsideAsteroidError
+        elif any([self.x == self.obstacle.x, self.y == self.obstacle.y]):
             raise ObstacleRobotError
 
     def turn_left(self):
         left_turns = {"E": "N", "N": "W", "W": "S", "S": "E"}
         self.direction = left_turns.get(self.direction, "N")
+        # if left_turns == {"E": "W", "N": "S", "W": "E", "S": "N"}:
+        #     raise DirectionRobotError
 
     def turn_right(self):
         right_turns = {"E": "S", "S": "W", "W": "N", "N": "E"}
         self.direction = right_turns.get(self.direction, "N")
+        # if right_turns == {"E": "W", "N": "S", "W": "E", "S": "N"}:
+        #     raise DirectionRobotError
 
     def move_forward(self):
         forward_moves = {
@@ -39,18 +45,32 @@ class Robot:
             "S": {self.position: (self.x, self.y - 1)},
             "W": {self.position: (self.x - 1, self.y)},
         }
-        self.position = forward_moves.get(self.position, None)
+        self.position = forward_moves.get(self.direction)
 
     def move_backward(self):
-        backward_moves = {self.position: self.x - 1 or self.y - 1}
-        self.position = backward_moves.get(self.position, None)
+        backward_moves = {
+            "N": {self.position: (self.x, self.y - 1)},
+            "E": {self.position: (self.x - 1, self.y)},
+            "S": {self.position: (self.x, self.y + 1)},
+            "W": {self.position: (self.x + 1, self.y)},
+        }
+        self.position = backward_moves.get(self.direction)
 
 
-class MissAsteroidError(Exception):
+class OutsideAsteroidError(Exception):
     def __str__(self):
-        return "Oops! Your robot missed from asteroid!"
+        return "Oops! Your robot can't be outside of the asteroid!"
+
+
+class ObstaclePositionError(Exception):
+    def __str__(self):
+        return "Error! Obstacles can't be outside of the asteroid!"
 
 
 class ObstacleRobotError(Exception):
     def __str__(self):
         return "Oops! Your robot has some obstacle on his way!"
+
+
+# class DirectionRobotError(Exception):
+#     pass
--- lantern/tdd/test_lonely_robot_execution.py
+++ lantern/tdd/test_lonely_robot_execution.py
@@ -3,8 +3,10 @@ from lonely_robot import (
     Robot,
     Asteroid,
     Obstacle,
-    MissAsteroidError,
     ObstacleRobotError,
+    # DirectionRobotError,
+    OutsideAsteroidError,
+    ObstaclePositionError,
 )
 
 
@@ -35,19 +37,36 @@ class TestRobotCreation:
         ),
     )
     def test_check_if_robot_on_asteroid(self, asteroid_size, robot_coordinates, obstacle_position):
-        with pytest.raises(MissAsteroidError):
+        with pytest.raises(OutsideAsteroidError):
             asteroid = Asteroid(*asteroid_size)
             obstacle = Obstacle(*obstacle_position, asteroid)
             Robot(*robot_coordinates, asteroid, "W", obstacle)
 
+    @pytest.mark.parametrize(
+        "asteroid_size,obstacle_position",
+        (
+                ((15, 25), (16, 26)),
+                ((15, 25), (16, 25)),
+                ((15, 25), (15, 26)),
+                ((15, 25), (-1, -1)),
+                ((15, 25), (12, -5)),
+                ((15, 25), (-7, 17)),
+        ),
+    )
+    def test_check_if_obstacle_on_asteroid(self, asteroid_size, obstacle_position):
+        with pytest.raises(ObstaclePositionError):
+            asteroid = Asteroid(*asteroid_size)
+            Obstacle(*obstacle_position, asteroid)
 
-class TestTurns:
+
+class TestTurnsAndMoves:
     def setup(self):
         self.a, self.b = 10, 15
         self.asteroid = Asteroid(self.a, self.b)
         self.x_obstacle, self.y_obstacle = 7, 11
         self.obstacle = Obstacle(self.x_obstacle, self.y_obstacle, self.asteroid)
-        self.x, self.y = 10, 15
+        self.x, self.y = 5, 10
+        self.direction = "N"
 
     @pytest.mark.parametrize(
         "current_direction,expected_direction",
@@ -77,8 +96,25 @@ class TestTurns:
         robot.turn_right()
         assert robot.direction == expected_direction
 
+    @pytest.mark.parametrize(
+        "current_direction,none_expected_direction",
+        (
+                ("N", "S"),
+                ("E", "W"),
+                ("S", "N"),
+                ("W", "E"),
+        ),
+    )
+    def test_if_robot_does_not_make_wrong_turns(self, current_direction, none_expected_direction):
+        # with pytest.raises(DirectionRobotError):
+            # obstacle = self.obstacle
+            # Robot(self.x, self.y, self.asteroid, current_direction, self.obstacle)
+        robot = Robot(self.x, self.y, self.asteroid, current_direction, self.obstacle)
+        robot.turn_right()
+        assert robot.direction != none_expected_direction
+        robot.turn_left()
+        assert robot.direction != none_expected_direction
 
-class TestMoves:
     @pytest.mark.parametrize(
         "direction,current_position,expected_position",
         (
@@ -89,16 +125,9 @@ class TestMoves:
         )
     )
     def test_move_forward(self, current_position, expected_position, direction):
-        robot = Robot(self.x, self.y, self.asteroid, current_position, self.obstacle)
+        robot = Robot(self.x, self.y, self.asteroid, self.direction, self.obstacle)
         robot.move_forward()
         assert robot.x, robot.y == expected_position
-
-    def setup(self):
-        self.a, self.b = 10, 15
-        self.asteroid = Asteroid(self.a, self.b)
-        self.x_obstacle, self.y_obstacle = 7, 11
-        self.obstacle = Obstacle(self.x_obstacle, self.y_obstacle, self.asteroid)
-        self.x, self.y = 10, 15
 
     @pytest.mark.parametrize(
         "direction,current_position,expected_position",
@@ -110,18 +139,36 @@ class TestMoves:
         )
     )
     def test_move_backward(self, current_position, expected_position, direction):
-        robot = Robot(self.x, self.y, self.asteroid, current_position, self.obstacle)
+        robot = Robot(self.x, self.y, self.asteroid, self.direction, self.obstacle)
         robot.move_backward()
         assert robot.x, robot.y == expected_position
 
     @pytest.mark.parametrize(
-        "current_position,expected_position,obstacle_position",
+        "direction,asteroid_size,current_position,expected_position",
         (
-                ((5, 10), (6, 10), (6, 10)),
-                ((5, 10), (5, 11), (5, 11))
+                ("N", (15, 25), (5, 25), (5, 26)),
+                ("E", (15, 25), (15, 10), (16, 10)),
+                ("S", (15, 25), (5, 0), (5, -1)),
+                ("W", (15, 25), (0, 10), (-1, 10)),
+        )
+    )
+    def test_check_if_robot_do_not_fail_from_asteroid(self, direction, asteroid_size, current_position,
+                                                      expected_position):
+        with pytest.raises(OutsideAsteroidError):
+            asteroid = Asteroid(*asteroid_size)
+            Robot(*expected_position, asteroid, "W", self.obstacle)
+
+    @pytest.mark.parametrize(
+        "direction,asteroid_size,current_position,expected_position,obstacle_position",
+        (
+                ("N", (15, 25), (5, 10), (5, 11), (5, 11)),
+                ("E", (15, 25), (4, 11), (5, 11), (5, 11)),
+                ("S", (15, 25), (5, 12), (5, 11), (5, 11)),
+                ("W", (15, 25), (6, 11), (5, 11), (5, 11)),
         ),
     )
-    def check_if_robot_have_no_obstacles(self, expected_position, obstacle_position):
+    def test_check_if_robot_have_no_obstacles(self, direction, asteroid_size, current_position, expected_position,
+                                              obstacle_position):
         with pytest.raises(ObstacleRobotError):
-            obstacle = Obstacle(*obstacle_position)
-            Robot(*expected_position, obstacle, "W")
+            asteroid = Asteroid(*asteroid_size)
+            Robot(*expected_position, asteroid, direction, self.obstacle)
