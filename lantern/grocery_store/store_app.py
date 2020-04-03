3bc13ed764c50b765dfe7ce2f0515602c6493bf0
BilyiVoron <aborsen1072@gmail.com>
2020-04-03 21:05:40 +0300

    Practice commited

--- lantern/grocery_store/store_app.py
+++ lantern/grocery_store/store_app.py
@@ -1,2 +1,21 @@
 from flask import Flask, jsonify, request
+import inject
+
+
 app = Flask(__name__)
+
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    db = inject.instance("DB")
+    user_id = db.users.add(request.json)
+    # __import__("pdb").set_trace()
+
+    return jsonify({"user_id": user_id})
+
+
+
+#
+#
+# if __name__ == "__main__":
+#     app.run()
