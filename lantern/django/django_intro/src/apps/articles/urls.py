d264facb1d29039fbcfa5da5ac3c725a5d4f2105
BilyiVoron <aborsen1072@gmail.com>
2020-06-10 01:17:33 +0300

    Added views and templates

--- lantern/django/django_intro/src/apps/articles/urls.py
+++ lantern/django/django_intro/src/apps/articles/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
 
-from apps.articles.views import main_page, SearchResultsView
+from apps.articles.views import (
+    main_page,
+    SearchResultsView,
+    main_page_logged_id,
+    ShowTitsView,
+    ArticleDetailView,
+    ArticleUpdateImageView,
+)
 
-app_name = 'articles'
+app_name = "articles"
 
 urlpatterns = [
-    path('search/', main_page, name='main-page'),
-    path('results/', SearchResultsView.as_view(), name='search-results')
+    path("search/<int:some_id>/", main_page_logged_id, name="main-page2"),
+    path("search/", main_page, name="main-page"),
+    path("results/", SearchResultsView.as_view(), name="search-results"),
+    path("tits_list/", ShowTitsView.as_view(), name="tits"),
+    path("<int:id>/", ArticleDetailView.as_view(), name="detail"),
+    path(
+        "<int:id>/change_image/", ArticleUpdateImageView.as_view(), name="update-image"
+    ),
 ]
