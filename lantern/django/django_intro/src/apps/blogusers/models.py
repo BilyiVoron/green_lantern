2bde708f0218bd555d4d271570042b89d2f90fce
BilyiVoron <aborsen1072@gmail.com>
2020-06-23 19:32:18 +0300

    Remove redundant code

--- lantern/django/django_intro/src/apps/blogusers/models.py
+++ lantern/django/django_intro/src/apps/blogusers/models.py
@@ -4,11 +4,3 @@ from django.db import models
 
 class BlogUser(AbstractUser):
     dob = models.DateField(null=True)
-
-
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-#     dob = models.DateField(null=True)
-#
-#     class Meta:
-#         abstract = True
