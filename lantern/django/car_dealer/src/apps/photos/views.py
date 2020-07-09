4656b8242489cb0107aa60f04488c4b2504ddd25
BilyiVoron <aborsen1072@gmail.com>
2020-07-09 21:08:21 +0300

    Added images' multiple uploading

--- lantern/django/car_dealer/src/apps/photos/views.py
+++ lantern/django/car_dealer/src/apps/photos/views.py
@@ -9,10 +9,17 @@ class UploadImageView(FormView):
     model = Photo
     form_class = ImageForm
     template_name = "image_uploading.html"
+    # pk_url_kwarg = "dealer_id"
 
     def get_success_url(self):
-        return reverse("cars_v1:cars")
+        return reverse("success")
 
-    # def form_valid(self, form):
-    #     form.save()
-    #     return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.dealer = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super(UploadImageView, self).get_form_kwargs()
+        kwargs["dealer_id"] = self.request.user.pk
+        return kwargs
